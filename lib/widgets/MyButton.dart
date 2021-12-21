b5ba1a2658804edc017f2e64db8c388b854b0dde
Mohamedtarekmetwally@gmail.com <51732423+MohamedTarekMetwally@users.noreply.github.com>
2021-12-21 17:26:56 +0200

    Update MyButton.dart

--- lib/widgets/MyButton.dart
+++ lib/widgets/MyButton.dart
@@ -1,5 +1,8 @@
+// ignore_for_file: prefer_const_constructors_in_immutables, use_key_in_widget_constructors, prefer_const_literals_to_create_immutables, deprecated_member_use, sized_box_for_whitespace, unused_import
+
 import 'package:flutter/material.dart';
 import 'package:ecommerece_app/screens/sign_up.dart';
+
 
 class MyButton extends StatelessWidget {
   final VoidCallback onPressed;
@@ -10,13 +13,13 @@ class MyButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 40,
+      height: 20,
       width: double.infinity,
       child: RaisedButton(
-        child: Text(name),
-        color: Colors.lightBlue,
-        onPressed: onPressed,
-      ),
+          child: Text(name),
+          color: Colors.grey,
+          onPressed: onPressed,
+        ),
     );
   }
 }
